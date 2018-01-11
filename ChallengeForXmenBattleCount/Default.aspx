ae5a762cc107c7eb6cce3b3fd83c717c42f696b5
gralls3 <32280272+gralls3@users.noreply.github.com>
2018-01-11 15:30:57 -0800

    Initial commit

---
+++ ChallengeForXmenBattleCount/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeForXmenBattleCount.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Most battles belong to:
+    
+        <asp:Label ID="mostResultLabel" runat="server"></asp:Label>
+    
+        <br />
+        Least battles belong to:
+    
+        <asp:Label ID="leastResultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            int maxBattlesIndex = 0;
+            int minBattlesIndex = 0;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (numbers[i] > numbers[maxBattlesIndex])
+                {maxBattlesIndex = i;}
+
+                if (numbers[i] < numbers[minBattlesIndex])
+                {minBattlesIndex = i;}
+            }
+
+            mostResultLabel.Text = names[maxBattlesIndex];
+            leastResultLabel.Text = names[minBattlesIndex];
+        }
+    }
+}
